ef9b883f632d03a65334d49bfea1e8c71acb5e7a
werner33 <jordan@codetrack.dev>
2022-08-01 13:33:11 -0400

    fix width of navbar to max of 800 px

--- src/layout/navbar/Navbar.js
+++ src/layout/navbar/Navbar.js
@@ -10,23 +10,25 @@ function Navbar() {
 
     return (
         <div className="navbar">
-            <div className="navbar__logo">
-                <Link to="/">
-                    Student App
-                </Link>
+            <div className="navbar__links">
+                <div className="navbar__logo">
+                    <Link to="/">
+                        Student App
+                    </Link>
+                </div>
+                <div className="navbar__menuItems">
+                    <li className="navbar__menuItem">
+                        <Link to="/">Students</Link>
+                    </li>
+                    <li className="navbar__menuItem">
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li className="navbar__menuItem">
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </div>
+                <div className="navbar__toggleIcon">=</div>
             </div>
-            <div className="navbar__menuItems">
-                <li className="navbar__menuItem">
-                    <Link to="/">Students</Link>
-                </li>
-                <li className="navbar__menuItem">
-                    <Link to="/about">About</Link>
-                </li>
-                <li className="navbar__menuItem">
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </div>
-            <div className="navbar__toggleIcon">=</div>
         </div>
     );
 }

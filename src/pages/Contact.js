7b6078c97010dcbf499164cd4d4edceb9f2ae1c3
werner33 <jordan@codetrack.dev>
2022-08-08 12:32:16 -0400

    complete contact page, including responsive design

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FaCheck } from 'react-icons/fa';
 
-
 import './Contact.scss';
 
 function Contact(props) {
@@ -42,8 +41,15 @@ function Contact(props) {
 
                 </div>
             </div>
-            <div className="contact__form">
-                Contact Form
+            <div className="contact__info">
+                <div className="contact__title">Get In Touch</div>
+
+                <div className="contact__subheader"><span>Phone: </span>583-284-9987</div>
+
+                <div className="contact__subheader">Address:</div>
+                <div className="contact__subheader">123 Sesame St <br/>Podunk, FL 49823</div>
+
+                <div className="contact__subheader"><span>Email:</span> writetome@gmail.com</div>
             </div>
         </div>
     );
